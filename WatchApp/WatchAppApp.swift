32b2bc2d86f0d025dd8ea8307a68ea570431add9
Akshay Khandizod <akshay.k@kiddopia.com>
2024-12-26 18:19:24 +0530

    Initial Commit

---
+++ WatchApp/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp
+//
+//  Created by Akshay Khandizod on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
